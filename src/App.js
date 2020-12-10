dbb20ae07c70e3e7ca7e6de4251c18c29af93f24
ulvi_199517 <ulvi199517@gmail.com>
2020-12-09 19:39:05 -0500

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route, Redirect} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions';
+import {createStructuredSelector} from 'reselect';
+import {selectCurrentUser} from './redux/user/user.reselect';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -33,18 +36,20 @@ class App extends React.Component {
     }
   render(){
     return (
-      <div className="App">
+      <div className="App"  
+      >
         <Header/>
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
+          <Route exact path='/checkout' component={CheckoutPage}/>
           <Route 
             exact 
             path='/signin' 
             render={
-              () => this.props.currentUser ? 
-              (<Redirect to='/' />) :
-              (<SignInAndSignUpPage/>)
+              () => this.props.currentUser 
+              ? (<Redirect to='/' />)
+              : (<SignInAndSignUpPage/>)
             } 
           />
         </Switch>
@@ -52,10 +57,10 @@ class App extends React.Component {
     );
   }
 }
-const mapStateToProps = ({user}) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 });
 const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+  setCurrentUser: user => dispatch(setCurrentUser(user)),
 });
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,6 +4,7 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-cart-origina
 import {connect} from 'react-redux';
 import {toggleCartHidden} from '../../redux/cart/cart.actions';
 import {selectCartItemsCount} from '../../redux/cart/cart.selectors';
+import {createStructuredSelector} from 'reselect';
 
 const CartIcon = ({toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
@@ -11,8 +12,8 @@ const CartIcon = ({toggleCartHidden, itemCount}) => (
         <span className='item-count'>{itemCount}</span>
     </div>
 );
-const mapStateToProps = (state) => ({
-    itemCount: selectCartItemsCount(state)
+const mapStateToProps = createStructuredSelector({
+    itemCount: selectCartItemsCount
 })
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
