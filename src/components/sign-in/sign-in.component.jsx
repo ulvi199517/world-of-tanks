d5f8348702aab964a74169751db46b1fa82197d5
ulvi_199517 <ulvi199517@gmail.com>
2020-12-20 22:28:13 -0500

    converted class components into functional with useState hook in sign in and sign up components

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './sign-in.styles.scss';
 import {FaFacebook, FaGoogle, FaEnvelope, FaGithub} from 'react-icons/fa';
 import FormInput from '../form-input/form-input.component';
@@ -11,58 +11,52 @@ import {
     } from '../../redux/user/user.actions';
 import {connect} from 'react-redux';
 
-class SignIn extends React.Component {
-    constructor(props){
-        super(props);
-
-        this.state = {
-            email: '',
-            password: ''
-        }
-    }
-    handleSubmit = async event => {
+const SignIn = ({emailSignInStart, googleSignInStart, facebookSignInStart, githubSignInStart}) => {
+    const [userCredentials, setUserCredentials] = useState({ 
+        email: '', 
+        password: ''
+    });
+    const {email, password} = userCredentials;
+    
+    const handleSubmit = async event => {
         event.preventDefault();
-        const {email, password} = this.state;
-        const {emailSignInStart} = this.props;
         emailSignInStart(email, password);
     }
-    handleChange = event => {
+    const handleChange = event => {
         const {value, name} = event.target;
 
-        this.setState({ [name]: value})
+        setUserCredentials({ ...userCredentials, [name]: value})
     }
-    render(){
-        const {googleSignInStart, facebookSignInStart, githubSignInStart} = this.props;
-        return(
-            <div className='sign-in'>
-                <h2 className='title'>I already have an account</h2>
-                <span className='subtitle'> Sign in with your email and password</span>
-                <form onSubmit={this.handleSubmit}>
-                    <FormInput 
-                        name='email'
-                        type='email'
-                        label='Email'
-                        value={this.state.email}
-                        handleChange={this.handleChange}
-                        required 
-                    />
-                    <FormInput 
-                        name='password'
-                        type='password'
-                        label='Password'
-                        value={this.state.password}
-                        handleChange={this.handleChange}
-                        required 
-                    />
-                    <CustomButton type='submit'><FaEnvelope className='envelope-icon'/> Sign in</CustomButton>
-                    <span className='divider'>or</span>
-                    <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn><FaGoogle className='google-icon'/> Sign in with Google</CustomButton>
-                    <CustomButton type='button' onClick={facebookSignInStart} isFacebookSignIn><FaFacebook className='facebook-icon'/> Sign in with Facebook</CustomButton>
-                    <CustomButton type='button' onClick={githubSignInStart} isGithubSignIn><FaGithub className='github-icon'/> Sign in with Github</CustomButton>
-                </form>
-            </div>
-        );
-    }
+
+    return(
+        <div className='sign-in'>
+            <h2 className='title'>I already have an account</h2>
+            <span className='subtitle'> Sign in with your email and password</span>
+            <form onSubmit={handleSubmit}>
+                <FormInput 
+                    name='email'
+                    type='email'
+                    label='Email'
+                    value={email}
+                    handleChange={handleChange}
+                    required 
+                />
+                <FormInput 
+                    name='password'
+                    type='password'
+                    label='Password'
+                    value={password}
+                    handleChange={handleChange}
+                    required 
+                />
+                <CustomButton type='submit'><FaEnvelope className='envelope-icon'/> Sign in</CustomButton>
+                <span className='divider'>or</span>
+                <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn><FaGoogle className='google-icon'/> Sign in with Google</CustomButton>
+                <CustomButton type='button' onClick={facebookSignInStart} isFacebookSignIn><FaFacebook className='facebook-icon'/> Sign in with Facebook</CustomButton>
+                <CustomButton type='button' onClick={githubSignInStart} isGithubSignIn><FaGithub className='github-icon'/> Sign in with Github</CustomButton>
+            </form>
+        </div>
+    );
 }
 const mapDispatchToProps = dispatch => ({
     googleSignInStart: () => dispatch(googleSignInStart()),
