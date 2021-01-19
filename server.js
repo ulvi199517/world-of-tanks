d26e0a174e076401f98bfbf3550836e375c0648d
ulvi_199517 <ulvi199517@gmail.com>
2021-01-18 21:26:30 -0500

    updating our coder to be PWA compliant, added service worker, updated manifest

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.listen(port, error => {
     if(error) throw error;
     console.log('Server running on port ' + port);
 });
-// app.get('./service-worker.js', (req,res) => {
-//     res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-// });
+app.get('./service-worker.js', (req,res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+});
 
 app.post('/payment', (req,res) => {
     const body = {
